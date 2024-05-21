c6924e354c0193f03e8f038cbb7719b25db9fa5f
ghugalsakshi <ghugalsakshi@gmail.com>
2024-05-21 17:27:54 +0530

    forget

--- routes.js
+++ routes.js
@@ -5,4 +5,12 @@ const dashboard = require('./dashboard/dashboard.js');
 //const SA = require('./superadmin/SA.js');
 
 
+router.post('/verify', authentication.verifyToken);
+router.post('/re-verify-mail', authentication.resendToken);
+router.post('/forgot', authentication.forgotPassword);
+router.post('/reset-password', authentication.resetPassword);
+router.post('/resend-forgot', authentication.resendResetToken);
+
+
+
 module.exports = router;
